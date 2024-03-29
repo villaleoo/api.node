f89db6d83e7ea0023dddbc17c840d8f43ec74b6f
leopoldo <villaleoo@hotmail.com>
2024-03-29 01:11:02 -0300

    deploy

--- app/models/taskModel.js
+++ app/models/taskModel.js
@@ -2,18 +2,18 @@
 const mysql = require('mysql2/promise');
 
 //create the connection to database
-const connectionPromise = mysql.createConnection({      /*haciendolo de esta manera, la conexion se dispara una sola vez y guarda el resultado en la constante */
-  host: 'localhost',
-  user: 'root',
-  password:'',
-  database: 'db-task'
-});
+// const connectionPromise = mysql.createConnection({      /*haciendolo de esta manera, la conexion se dispara una sola vez y guarda el resultado en la constante */
+//   host: 'localhost',
+//   user: 'root',
+//   password:'',
+//   database: 'db-task'
+// });
 
 
 
 function getAll(){
 
-    
+
 
     console.log("bienvenido al modelo de tasks - getAll");
 }
